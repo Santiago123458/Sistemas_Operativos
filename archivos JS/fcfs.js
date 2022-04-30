9cc9723188003bce92845ffaaf9db2c53ae60b57
Oscar Saavedra <osegos10@gmail.com>
2022-04-29 19:42:13 -0500

    FIFo 2.0

--- archivos JS/fcfs.js
+++ archivos JS/fcfs.js
@@ -1,21 +1,61 @@
 (() => {
+    // Almacena los proceso ingresados por el usuario
+    let procesos = [] 
+
+    // Clase para crear los procesos 
     class Proceso{
 
-        _nombre = ''
-        _activo = ''
-        _duracion = ''
-        _llegada = ''
-    
-        constructor(nombre = 'Sin nombre', activo = true, duracion = -1, llegada = -1 ){
+        _nombre = '' //A , B ....
+        _tiempoTotal = 0 // Tiempo total que tomo el proceso para ejecutarse (tiempo espera y trabajo util)
+        _duracionProceso = 0 // Tiempo que necesita el proceso para ejecutarse [ticks]
+        _llegada = 0 // Instante del tiempo en el que llega el proceso [ticks]
+        _penalizacion = 0 //
+        _tiempoEspera = 0 // tiempo que espera el proeso
+        _inicio = 0 // Cunado inicia a ejecutarse el proceso
+        _fin = 0 // Cuando finaliza de ejecutarse el proceso 
+
+        constructor(nombre = 'Sin nombre', tiempoTotal = 0,  duracionProceso = 0, llegada = 0, penalizacion = 0, tiempoEspera = 0, inicio = 0, fin = 0 ){
             this._nombre = nombre
-            this._activo = activo
-            this._duracion = duracion
+            this._tiempoTotal = tiempoTotal
+            this._duracionProceso = duracionProceso
             this._llegada = llegada
+            this._penalizacion = penalizacion
+            this.tiempoEspera = tiempoEspera
+            this._inicio = inicio
+            this._fin = fin
         }
 
         set nombre (nombre){
-            this._nombre = nombre.toLowerCase()
+            this._nombre = nombre.toUpperCase()
         } 
+
+        set tiempoTotal(tiempoTotal){
+            this._tiempoTotal = tiempoTotal
+        }
+        
+        set duracionProceso (duracionProceso){
+            this._duracionProceso = duracionProceso
+        }
+
+        set llegada(llegada){
+            this._llegada = llegada
+        }
+
+        set penalizacion(penalizacion){
+            this._penalizacion = penalizacion
+        }
+
+        set tiempoEspera(tiempoEspera){
+            this._tiempoEspera = tiempoEspera
+        }
+
+        set inicio(inicio){
+            this._inicio = inicio
+        }
+
+        set fin(fin){
+            this._fin = fin
+        }
 
         get nombre(){
             return this._nombre
@@ -27,6 +67,18 @@
     
     }
     
+    // Organiza los procasos por orden de llegada
+    const organizarProcesos = () => {
+        procesos = procesos.sort(function(a, b){
+            return a - b
+        })
+    }
+
+    // inicia el algoritmo que simula FIFO 
+    function ejecutarAlgoritmo(){
+
+    }
+
     const proceso1 = new Proceso()
     proceso1.mostrarProceso()
     proceso1.nombre = 'Oscar'
